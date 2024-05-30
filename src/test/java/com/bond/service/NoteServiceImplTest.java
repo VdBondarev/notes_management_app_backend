9ebb298b78ec989f62f58e462367e2803e36618b
Vlad Bondarev <bondarev.21@icloud.com>
2024-05-30 16:18:50 +0300

    fully implemented unit tests for NoteService

--- src/test/java/com/bond/service/NoteServiceImplTest.java
+++ src/test/java/com/bond/service/NoteServiceImplTest.java
@@ -3,6 +3,7 @@ package com.bond.service;
 import static java.time.LocalDateTime.now;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
@@ -22,6 +23,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Example;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
@@ -229,6 +231,50 @@ class NoteServiceImplTest {
         String actualMessage = exception.getMessage();
 
         assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    @DisplayName("""
+            Verify that search() method works as expected with valid params
+            """)
+    public void search_ValidParams_ReturnsValidList() {
+        Note firstExpectedNote = new Note()
+                .setId(1L)
+                .setTitle("First test title")
+                .setContent("First test content")
+                .setCreatedAt(now())
+                .setLastUpdatedAt(now());
+
+        Note secondExpectedNote = new Note()
+                .setId(2L)
+                .setTitle("Second test title")
+                .setContent("Second test content")
+                .setCreatedAt(now())
+                .setLastUpdatedAt(now());
+
+        List<Note> expectedNoteList;
+        expectedNoteList = List.of(firstExpectedNote, secondExpectedNote);
+
+        Pageable pageable = PageRequest.of(0, 5);
+
+        Page<Note> page = new PageImpl<>(expectedNoteList, pageable, expectedNoteList.size());
+
+        NoteResponseDto firstResponseDto = createResponseDtoFromModel(firstExpectedNote);
+        NoteResponseDto secondResponseDto = createResponseDtoFromModel(secondExpectedNote);
+
+        when(noteRepository.findAll(any(Example.class), any(Pageable.class))).thenReturn(page);
+        when(noteMapper.toResponseDto(firstExpectedNote)).thenReturn(firstResponseDto);
+        when(noteMapper.toResponseDto(secondExpectedNote)).thenReturn(secondResponseDto);
+
+        NoteRequestDto requestDto = new NoteRequestDto("test", "test");
+
+        List<NoteResponseDto> actualList = noteService.search(requestDto, pageable);
+        List<NoteResponseDto> expectedList = List.of(firstResponseDto, secondResponseDto);
+
+        assertEquals(expectedList, actualList);
+
+        verify(noteMapper, times(2)).toResponseDto(any());
+        verifyNoMoreInteractions(noteMapper);
     }
 
     private NoteResponseDto createResponseDtoFromModel(Note note) {
