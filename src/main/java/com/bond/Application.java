4dc5886b305083793740a37a3f32f8ac632c9ef5
Vlad Bondarev <bondarev.21@icloud.com>
2024-05-30 11:06:43 +0300

    Initial commit

---
+++ src/main/java/com/bond/Application.java
@@ -0,0 +1,13 @@
+package com.bond;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesManagementAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotesManagementAppApplication.class, args);
+    }
+
+}
